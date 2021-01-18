3535d130e5c3a5e1299496e242ed8d90dea1394c
Christoph Krüger <christoph@opencampus.sh>
2021-01-18 11:42:36 +0100

    docusaurus generated code

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,8 @@
+export default [
+  require("/home/myrre/docusaurus/edu-plattform-docu/node_modules/remark-admonitions/styles/infima.css"),
+  require("/home/myrre/docusaurus/edu-plattform-docu/node_modules/remark-admonitions/styles/infima.css"),
+  require("/home/myrre/docusaurus/edu-plattform-docu/node_modules/remark-admonitions/styles/infima.css"),
+  require("/home/myrre/docusaurus/edu-plattform-docu/node_modules/infima/dist/css/default/default.css"),
+  require("/home/myrre/docusaurus/edu-plattform-docu/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/myrre/docusaurus/edu-plattform-docu/src/css/custom.css"),
+];
