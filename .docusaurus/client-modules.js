45ba4e93b503b18d2497ac65a605adfec66c36e2
freerafiki <bagigi@disroot.org>
2021-11-18 13:22:58 +0100

    removing old one

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/Users/Palma/Documents/Githubs/edu-plattform-docu/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/Palma/Documents/Githubs/edu-plattform-docu/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/Palma/Documents/Githubs/edu-plattform-docu/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/Palma/Documents/Githubs/edu-plattform-docu/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/Palma/Documents/Githubs/edu-plattform-docu/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/Palma/Documents/Githubs/edu-plattform-docu/src/css/custom.css"),
-];
