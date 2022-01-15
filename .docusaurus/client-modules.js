f8c317ee761400cc15cfa1b9ba0998928eb717ca
freerafiki <bagigi@disroot.org>
2022-01-16 00:10:28 +0100

    removing/ignoring files

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/Palma/Documents/Opencampus/edu-hub-project/documentation/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/Palma/Documents/Opencampus/edu-hub-project/documentation/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/Palma/Documents/Opencampus/edu-hub-project/documentation/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/Users/Palma/Documents/Opencampus/edu-hub-project/documentation/src/css/custom.css"),
-];
